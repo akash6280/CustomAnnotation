4b6b7cef4858fed6402b4685c4d8ad5fdf58283e
Akash Singh <akashsingh6280@gmail.com>
2021-09-21 00:50:25 +0530

    ADDED: Class that demontrates custom annotations

---
+++ src/main/java/com/bridgelabz/customannotation/AnnotationExample.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.customannotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class AnnotationExample 
+{
+	@Override
+	@MethodInfo(author = "akash", comments = "Main mathod", date = "Nov 17 2012", revision = 1)
+	public String toString() {
+		return "Overrided toString method";
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			for(Method method: AnnotationExample.class.getMethods()) {
+				if(method.isAnnotationPresent(MethodInfo.class)) {
+					try {
+						for(Annotation anno : method.getDeclaredAnnotations()) {
+							System.out.println("Annotation in method: "+method+" : "+anno);
+						}
+						MethodInfo methodAnno = method.getAnnotation(MethodInfo.class);
+						if(methodAnno.revision()==1) {
+							System.out.println("Method with revision 1: "+method);
+						}
+					}
+					catch (Throwable e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		catch (SecurityException e){
+			e.printStackTrace();
+		}
+	}
+
+}
